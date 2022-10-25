760fd7a381735f219b1c85cf6e6aad8760c72ceb
Herbert Ferreira da Silva azevedo <112709078+kakarotofla@users.noreply.github.com>
2022-10-25 14:24:45 -0300

    implementação da segunça de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/entities/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
 import { Tema } from './tema/entities/tema.entity';
