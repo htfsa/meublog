756535ef20333a6efbd5d5e96d8bcc9cd8c37317
Herbert Ferreira da Silva azevedo <112709078+kakarotofla@users.noreply.github.com>
2022-12-19 10:20:43 -0300

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,20 +12,20 @@ import { UsuarioModule } from './usuario/usuario.module';
 //isto roda no navegador
 @Module({
   imports: [
-     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'meublog',
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true
-
-    }),
+    //  TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'root',
+    //   database: 'meublog',
+    //   entities: [Postagem, Tema, Usuario],
+    //   synchronize: true
+ 
+    // }),
 
     //isto roda no servidor do vscode
-    /* TypeOrmModule.forRoot({
+    TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
       logging:false,
@@ -36,7 +36,7 @@ import { UsuarioModule } from './usuario/usuario.module';
       synchronize:true,
       autoLoadEntities:true
 
-    }), */
+    }), 
     PostagemModule,
     TemaModule,
     AuthModule,
